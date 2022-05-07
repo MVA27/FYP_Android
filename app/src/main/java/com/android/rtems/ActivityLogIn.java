55220fc492dae08d23ff35dc070b021435cd158e
MVA27 <manvekar906@gmail.com>
2022-05-08 02:29:56 +0530

    Implemented Register User Activity UI

--- app/src/main/java/com/android/rtems/ActivityLogIn.java
+++ app/src/main/java/com/android/rtems/ActivityLogIn.java
@@ -4,23 +4,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ActivityLogIn extends AppCompatActivity {
 
-    Button login;
+    Button buttonLogIn;
+    ImageView imageViewRegisterUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        login = findViewById(R.id.log_in_button_id);
-        login.setOnClickListener(new View.OnClickListener() {
+        buttonLogIn = findViewById(R.id.id_log_in_button);
+        imageViewRegisterUser = findViewById(R.id.id_login_register_user_button);
+
+        buttonLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(ActivityLogIn.this,ActivityDisplay.class);
+                startActivity(intent);
+            }
+        });
+
+        imageViewRegisterUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ActivityLogIn.this,ActivityRegisterUser.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/android/rtems/ActivityRegisterUser.java
@@ -0,0 +1,16 @@
+package com.android.rtems;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityRegisterUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+
+
+    }
+}

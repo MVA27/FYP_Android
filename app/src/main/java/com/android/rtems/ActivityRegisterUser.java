d11310683e7c6f5bdc2ff8afb1c13aef2d3ca44c
MVA27 <manvekar906@gmail.com>
2022-05-09 20:19:55 +0530

    feat : phone number field added in ActivityRegisterUser.java

--- app/src/main/java/com/android/rtems/ActivityRegisterUser.java
+++ app/src/main/java/com/android/rtems/ActivityRegisterUser.java
@@ -10,7 +10,5 @@ public class ActivityRegisterUser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
-
-
     }
 }
--- app/src/main/java/com/android/rtems/Threads/VerifyUser.java
+++ app/src/main/java/com/android/rtems/Threads/VerifyUser.java
@@ -22,7 +22,6 @@ public class VerifyUser extends Thread{
     Handler handler;
     ProgressBar progressBar;
 
-
     public VerifyUser(Context context,Intent intent,Editable userName,Editable password,Handler handler,ProgressBar progressBar){
         this.context = context;         //To call startActivity() method from Thread (Non-Activity class)
         this.intent = intent;           //To move to new Activity

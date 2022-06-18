692a7e57edbdad298357ba043c65a98f64fe2e26
MVA27 <manvekar906@gmail.com>
2022-06-18 20:03:33 +0530

    feat : Redesigned ActivitySettings UI

--- app/src/main/java/com/android/rtems/ActivitySettings.java
+++ app/src/main/java/com/android/rtems/ActivitySettings.java
@@ -10,7 +10,7 @@ import com.android.rtems.storage.Static;
 public class ActivitySettings extends AppCompatActivity {
 
     Handler handler = new Handler();
-    EditText temperature,pressure,humidity,airQuality;
+    EditText temperature,pressure,humidity,airQuality,refreshTimer;
     ProgressBar progressBar;
 
     @Override
@@ -22,6 +22,7 @@ public class ActivitySettings extends AppCompatActivity {
         pressure = findViewById(R.id.id_settings_edit_text_pressure);
         humidity = findViewById(R.id.id_settings_edit_text_humidity);
         airQuality = findViewById(R.id.id_settings_edit_text_airquality);
+        refreshTimer = findViewById(R.id.id_settings_edit_text_refreshtimer);
         progressBar = findViewById(R.id.id_settings_progress_bar);
 
         handler.post(new Runnable() {
@@ -36,10 +37,11 @@ public class ActivitySettings extends AppCompatActivity {
                     }
                 }
 
-                temperature.setText("Temperature "+Static.threshold.getTemperature());
-                pressure.setText("Pressure "+Static.threshold.getPressure());
-                humidity.setText("Humidity "+Static.threshold.getHumidity());
-                airQuality.setText("Air Quality "+Static.threshold.getAir_quality());
+                temperature.setText(String.valueOf(Static.threshold.getTemperature()));
+                pressure.setText(String.valueOf(Static.threshold.getPressure()));
+                humidity.setText(String.valueOf(Static.threshold.getHumidity()));
+                airQuality.setText(String.valueOf(Static.threshold.getAir_quality()));
+                refreshTimer.setText(String.valueOf(Static.refreshTime));
             }
         });
     }
